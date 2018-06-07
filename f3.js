273cf46bfc2cd478db2d76a5d5f83e58f011b4bd
kreshchenko <kreshchenkoi@gmail.com>
2018-06-07 17:21:53 +0300

    f3() - новая реализация функции

--- f3.js
+++ f3.js
@@ -1,70 +1,30 @@
-"use strict";
+function f3(line) {
+  //разделяем строку на подстроки
+  //с помощью reduce идем по массиву подстрок
+  return line.split("&").reduce((result, item) => {
+    let [excess, keys, value] = item.match(/([^=]*)=\"?([^\"]*)\"?$/); //разделяем подстроку на ключи и значение
 
-/*Функция создания обьекта из строки */
-function f2(str) {
-  /*Разделяем строку на ключ и значение */
-  let strArr = str.split("=");
-  /*Получаем массив значений ключа/ключей из строки*/
-  let keys = strArr[0].split(".");
-  /*Получаем значение и удаляем с него кавычки если они есть*/
-  let value = strArr[1].replace(/"/g, "");
-
-  let i = 1;
-  let obj = {};
-
-  if (value.trim() != "") {
-    keys.forEach(key => {
-      obj = {
-        [keys[keys.length - i]]: value
-      };
-      value = obj;
-      i++;
-    });
-  }
-
-  return obj;
-}
-
-function f3(str) {
-  /*Разделение строки на подстроки */
-  let stringsArr = str.split("&");
-
-  /*Массив для хранения обьектов полученых с строки*/
-  let objArr = [];
-
-  /*Превращаем каждую строку в обьект с помощью функции f2 из прошлой задачи */
-  stringsArr.forEach(someString => {
-    objArr.push(f2(someString));
-  });
-
-  /*Индекс для уменьшения прохода по массиву при поиске одинаковых */
-  let start = 0;
-  /*Поиск одинаковых ключей и обьединение обьектов */
-  objArr.forEach((someObj, index) => {
-    let key = Object.keys(someObj); //получаем ключ который будем проверять на повторение
-    for (let i = start; i < objArr.length; i++) {
-      let tmpKey = Object.keys(objArr[i]); //ключ для сравнения с key
-      if (key.toString() == tmpKey.toString() && index != i) {
-        objArr[index][key] = Object.assign(objArr[index][key], objArr[i][key]); //Объдинение обьектов
-        objArr.splice(i, 1); //удаляем елемент который обьединяли
-      }
+    if (!value) {
+      return result;
     }
-    start++;
-  });
 
-  /*Создаем объект результат */
-  let resultObj = {};
-  objArr.forEach(someObj => {
-    resultObj = Object.assign(resultObj, someObj);
-  });
+    keys = keys.split("."); //разделяем ключи на подключи
 
-  return resultObj;
+    let lastLevel = result,
+      lastKey = keys.pop(); //записіваем ключ самого нижнего уровня и удаляем его
+
+    //Проход по массиву ключей
+    keys.forEach(function(key) {
+      lastLevel[key] = lastLevel[key] || {}; //Создаем пустой обьект для дочернего уровня, если не біло раньше
+      lastLevel = lastLevel[key]; // ссылка на только что созданый или существующий дочерний обьект
+    });
+    lastLevel[lastKey] = value; //Записываем значение в обьект самого нижнего уровня
+
+    return result;
+  }, {});
 }
 
-let str = "foo.id=92499aacd96553f313fb9b85913f2e18&boo.foo=10&foo.bar=sdds";
-let str2 = "foo.id=92499aacd96553f313fb9b85913f2e18&boo.foo=10&foo.bar=";
-let str3 = "user.name=Alex&user.name=Oleg";
-let str4 = "db.host=&db.user=root&db.pass=12340";
+let str = 'db.host=&db.user="root"&db.pass=1234';
+let obj = f3(str);
 
-let a = f3(str);
-console.log(a.foo.bar);
+console.log(obj);
